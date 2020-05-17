4062aebf5bb313ab4584331a5c9bd411a91d2e0a
Jeremy <jeremy.christian.san@gmail.com>
2020-05-17 22:44:48 +0100

    add text-overflow

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const url = "mongodb://localhost:27017";
 const dbName = "myproject";
 
 const saveNewGame = (game, socket) =>
-  MongoClient.connect(url, function(err, client) {
+  MongoClient.connect(url, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
@@ -27,8 +27,8 @@ const saveNewGame = (game, socket) =>
     client.close();
   });
 
-const pushGames = socket =>
-  MongoClient.connect(url, function(err, client) {
+const pushGames = (socket) =>
+  MongoClient.connect(url, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
@@ -36,7 +36,7 @@ const pushGames = socket =>
     // Get the games collection
     const collection = db.collection("games");
     // Find some documents
-    collection.find({}).toArray(function(err, games) {
+    collection.find({}).toArray(function (err, games) {
       assert.equal(err, null);
       console.log("Found the records");
       socket.emit("pushGames", games);
@@ -44,10 +44,10 @@ const pushGames = socket =>
     client.close();
   });
 
-io.on("connection", function(socket) {
+io.on("connection", function (socket) {
   pushGames(socket);
   console.log("a user connected");
-  socket.on("disconnect", function() {
+  socket.on("disconnect", function () {
     console.log("user disconnected");
   });
 
@@ -59,7 +59,7 @@ io.on("connection", function(socket) {
   });
 });
 
-server.listen(8000, function(err) {
+server.listen(8000, function (err) {
   if (err) throw err;
   console.log("listening on port 8000");
 });
